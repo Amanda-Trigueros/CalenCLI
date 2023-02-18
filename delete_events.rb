b361fcef7e6791e16e4092507ca52de1c9ef7702
Amanda Trigueros <amandatrigueros01@gmail.com>
2023-02-17 19:56:52 -0600

    add: update delete events

--- delete_events.rb
+++ delete_events.rb
@@ -2,17 +2,12 @@ require "date"
 require_relative "list_events"
 
 def delete_events(ids, events)
-    events.delete_if do |event|
-        ids.include?(event[:id])
-    end
+  events.delete_if do |event|
+    ids.include?(event[:id])
+  end
 end
 
-def grab_ids()
-    print "Event ID(s): "
-    gets.chomp.split(",").map(&:to_i)
-  end
-
-
-
-
-  
+def grab_ids
+  print "Event ID(s): "
+  gets.chomp.split(",").map(&:to_i)
+end
