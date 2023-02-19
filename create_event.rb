a6b15dc1204b9d97d273d77795c1a363a484e415
Amanda Trigueros <amandatrigueros01@gmail.com>
2023-02-18 22:19:42 -0600

    refactor: create & update

--- create_event.rb
+++ create_event.rb
@@ -1,70 +1,13 @@
-def valid_date?(date)
-  valid_format = "%Y-%m-%d"
-  !!(date.match(/\d{4}-\d{2}-\d{2}/) && DateTime.strptime(date, valid_format))
-rescue ArgumentError
-  false
-end
-
-def valid_calendar?(calendar)
-  valid_calendars = ["", "web-dev", "english", "soft-skills"]
-  valid_calendars.include?(calendar)
-end
-
-def valid_start_end?(start_end)
-  return true if start_end == ""
-
-  valid_range = true
-  start_hour, end_hour = start_end.split
-  start_hour = start_hour.split(":")
-  end_hour = end_hour.split(":")
-  if start_hour[0] == end_hour[0]
-    valid_range = start_hour[1] < end_hour[1]
-  elsif start_hour[0] > end_hour[0]
-    valid_range = false
-  else
-    valid_range = true
-  end
-  !!(start_end.match(/\d{2}:\d{2} \d{2}:\d{2}/) && valid_range)
-rescue ArgumentError
-  false
-end
+require_relative "helper_methods"
 
 def create_event(events, id)
-  print "Date (YYYY-MM-DD): "
-  date = gets.chomp
-  until valid_date?(date)
-    puts "Invalid Date"
-    print "Date (YYYY-MM-DD): "
-    date = gets.chomp
-  end
-
-  print "Title: "
-  title = gets.chomp
-  while title.empty?
-    puts "Title shouldn't be empty"
-    print "Title: "
-    title = gets.chomp
-  end
-
-  print "Calendar: "
-  calendar = gets.chomp.strip
-  until valid_calendar?(calendar)
-    puts "Valid calendars: web-dev, english, soft-skills or defeault"
-    print "Calendar: "
-    calendar = gets.chomp.strip
-  end
-
-  print "Start_End (HH:MM HH:MM): "
-  start_end = gets.chomp
-  until valid_start_end?(start_end)
-    puts "Invalid format or invalid range, format should be HH:MM HH:MM"
-    print "Start_End (HH:MM HH:MM): "
-    start_end = gets.chomp
-  end
+  date = obtain_valid_date
+  title = obtain_valid_title
+  calendar = obtain_valid_calendar
+  start_end = obtain_valid_start_end
 
   print "Notes: "
   notes = gets.chomp
-
   print "Guests: "
   guests = gets.chomp.split(", ")
 
@@ -77,14 +20,6 @@ def create_event(events, id)
     end_date_format = "#{date}T#{end_our}:00-05:00"
   end
 
-  new_event = {
-    id:,
-    start_date: start_date_format,
-    title:,
-    end_date: end_date_format,
-    notes:,
-    guests:,
-    calendar:
-  }
+  new_event = { id:, start_date: start_date_format, title:, end_date: end_date_format, notes:, guests:, calendar: }
   events.push(new_event)
 end
--- update_event.rb
+++ update_event.rb
@@ -1,6 +1,6 @@
-require_relative "create_event"
+require_relative "helper_methods"
 
-def update(events, id)
+def get_valid_event_index(events, id)
   event_index = events.index { |event| event[:id] == id }
   while event_index.nil?
     puts "ID not found"
@@ -8,38 +8,14 @@ def update(events, id)
     id = gets.chomp.to_i
     event_index = events.index { |event| event[:id] == id }
   end
+end
 
-  print "Date (YYYY-MM-DD): "
-  date = gets.chomp
-  until valid_date?(date)
-    puts "Invalid Date"
-    print "Date (YYYY-MM-DD): "
-    date = gets.chomp
-  end
-
-  print "Title: "
-  title = gets.chomp
-  while title.empty?
-    puts "Title shouldn't be empty"
-    print "Title: "
-    title = gets.chomp
-  end
-
-  print "Calendar: "
-  calendar = gets.chomp.strip
-  until valid_calendar?(calendar)
-    puts "Valid calendars: web-dev, english, soft-skills or defeault"
-    print "Calendar: "
-    calendar = gets.chomp.strip
-  end
-
-  print "Start_End (HH:MM HH:MM): "
-  start_end = gets.chomp
-  until valid_start_end?(start_end)
-    puts "Invalid format or invalid range, format should be HH:MM HH:MM"
-    print "Start_End (HH:MM HH:MM): "
-    start_end = gets.chomp
-  end
+def update(events, id)
+  event_index = get_valid_event_index(events, id)
+  date = obtain_valid_date
+  title = obtain_valid_title
+  calendar = obtain_valid_calendar
+  start_end = obtain_valid_start_end
 
   print "Notes: "
   notes = gets.chomp
